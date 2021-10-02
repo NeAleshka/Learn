8b58c5a76b1ffcc7b42fa7f597ff3ae4ad2b47c2
Алексей <alexeybudai@gmail.com>
2021-10-02 22:48:28 +0300

    Array

--- app/src/main/java/com/example/learn/MainActivity.kt
+++ app/src/main/java/com/example/learn/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.learn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val lostArray= arrayOf(1000,1500,500,3000,5000,2000)
+    val profitArray= arrayOf(500,15000,2500,600,1000,7700)
+    val resultArray= ArrayList<String>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    val names=resources.getStringArray(R.array.nameArray)
+    for ((index,name)in names.withIndex()){
+        resultArray.add("Имя :$name - прибыль=${profitArray[index]-lostArray[index]}")
+    Log.d("MyLog", "Статистика -/- ${resultArray[index]}")
     }
+}
 }
