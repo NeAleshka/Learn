93ec5224912adb0c6c694e02517416f981eebc4c
Алексей <alexeybudai@gmail.com>
2021-10-04 23:21:59 +0300

    Пифагор

--- app/src/main/java/com/example/learn/MainActivity.kt
+++ app/src/main/java/com/example/learn/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.learn.databinding.ActivityMainBinding
 import kotlin.math.pow
 import kotlin.math.sqrt
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     private val mainActivity: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
