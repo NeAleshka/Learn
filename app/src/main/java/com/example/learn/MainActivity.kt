63a59cb1879f00e68e07ccdcf0093b676cae525d
Алексей <alexeybudai@gmail.com>
2021-10-04 23:21:13 +0300

    Array

--- app/src/main/java/com/example/learn/MainActivity.kt
+++ app/src/main/java/com/example/learn/MainActivity.kt
@@ -1,31 +1,39 @@
 package com.example.learn
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import kotlin.math.log
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.learn.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val mainActivity: ActivityMainBinding by viewBinding()
 
-
-class MainActivity : AppCompatActivity() {
-    lateinit var launch:ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        launch=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result:ActivityResult->{
-
+        mainActivity.button.setOnClickListener {
+            if(!isFieldEmpty()){
+            val result=getString(R.string.textRes)+getResult()
+            mainActivity.tVResult.text=result
+                Log.d("MyLog",result)
+            }
         }
-        }
-        var user=User("Alex", "123", 13)
-        var user2=User("Andrey", "11", 22)
-        user.addAge(10)
-        user2.addAge(-2)
-        user.log()
-        user2.log()
-
-
     }
+    private fun isFieldEmpty():Boolean {
+        mainActivity.apply {
+        if(edA.text.isNullOrEmpty()) edA.error=getText(R.string.masErrorA)
+        if(edB.text.isNullOrEmpty()) edB.error=getText(R.string.masErrorB)
+            return edA.text.isNullOrEmpty()|| edB.text.isNullOrEmpty()
+        }
+    }
+private fun getResult():String{
+    val a:Double
+    val b:Double
+    mainActivity.apply {
+        a=edA.text.toString().toDouble()
+        b=edB.text.toString().toDouble()
+    }
+    val result=sqrt((a.pow(2))+(b.pow(2)))
+    return ("%.2f".format(result))
+}
 }
--- app/src/main/java/com/example/learn/User.kt
+++ app/src/main/java/com/example/learn/User.kt
@@ -5,6 +5,7 @@ import android.util.Log
 class User( var name:String, var id:String, var age:Int) {
     fun addAge(years:Int){
     age=age.plus(years)
+    log()
     }
 fun log(){
     Log.d("MyLog","Name:$name id:$id age:$age")
